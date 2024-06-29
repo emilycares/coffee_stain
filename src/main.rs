2606581db79f81a776e992c1b520dfec03c446cb
micmine <micmine4@gmail.com>
2024-06-29 09:19:48 +0200

    Improve cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::io;
+use std::io::BufRead;
 
 fn main() {
-    let stdin = io::stdin();
     eprintln!("Please paste the line that includes: \n  - \"org.opentest4j.AssertionFailedError: expected: <*> but was: <*>\" \n");
-    let text = io::read_to_string(stdin).expect("Can not read stdin");
-    if let Some(message) = coffee_stain::get_hint(&text, true) {
-        println!("{}", message);
+
+    let lines = std::io::stdin().lock().lines();
+    for line in lines {
+        if let Ok(line) = line {
+            if let Some(message) = coffee_stain::get_hint(&line, true) {
+                println!("{}", message);
+            }
+        }
     }
 }
